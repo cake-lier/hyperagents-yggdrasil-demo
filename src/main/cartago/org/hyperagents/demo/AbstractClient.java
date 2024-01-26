efbb7716ae044ba8f56b3420d3ee7b7e03f0e555
Matteo Castellucci <matteo.castellucci@outlook.com>
2024-01-26 14:39:42 +0100

    feat: add callback server and Yggdrasil clients implementations

---
+++ src/main/cartago/org/hyperagents/demo/AbstractClient.java
@@ -0,0 +1,123 @@
+package org.hyperagents.demo;
+
+import cartago.Artifact;
+import cartago.GUARD;
+import cartago.INTERNAL_OPERATION;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.model.impl.LinkedHashModel;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFHandlerException;
+import org.eclipse.rdf4j.rio.RDFParseException;
+import org.eclipse.rdf4j.rio.Rio;
+import org.eclipse.rdf4j.rio.helpers.BasicWriterSettings;
+import org.eclipse.rdf4j.rio.helpers.StatementCollector;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+
+public abstract class AbstractClient extends Artifact {
+    private WebClient client;
+    private String mailboxUri;
+    private String platformUri;
+    private String agentName;
+    private boolean requestComplete;
+
+    protected void init(final String agentName, final String mailboxUri, final String platformUri) {
+        this.client = WebClient.create(Vertx.vertx());
+        this.agentName = agentName;
+        this.mailboxUri = mailboxUri;
+        this.platformUri = platformUri;
+    }
+
+    protected final WebClient getClient() {
+        return this.client;
+    }
+
+    protected final String getMailboxUri() {
+        return this.mailboxUri;
+    }
+
+    protected final String getPlatformUri() {
+        return this.platformUri;
+    }
+
+    protected final String getAgentId() {
+        return this.platformUri + "/agent/" + this.agentName;
+    }
+
+    protected final String doRequest(final Future<HttpResponse<Buffer>> request) {
+        this.requestComplete = false;
+        request.onComplete(r -> this.execInternalOp("signalResponseReceived"));
+        this.await("isRequestComplete");
+        if (request.succeeded() && request.result().statusCode() <= 299) {
+            return request.result().bodyAsString();
+        } else {
+            this.failed(
+                request.result().statusCode() > 299
+                ? request.result().statusMessage()
+                : request.cause().getMessage()
+            );
+            throw new RuntimeException();
+        }
+    }
+
+    protected final Model parseFromTurtle(final String graphString) {
+        try (var stringReader = new StringReader(graphString)) {
+            final var rdfParser = Rio.createParser(RDFFormat.TURTLE);
+            final var model = new LinkedHashModel();
+            rdfParser.setRDFHandler(new StatementCollector(model));
+            rdfParser.parse(stringReader);
+            return model;
+        } catch (final RDFParseException | RDFHandlerException | IOException e) {
+            this.failed(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected final String parseToTurtle(final Model graph) {
+        try (var out = new ByteArrayOutputStream()) {
+            final var writer = Rio.createWriter(RDFFormat.TURTLE, out);
+            writer.getWriterConfig()
+                  .set(BasicWriterSettings.PRETTY_PRINT, true)
+                  .set(BasicWriterSettings.RDF_LANGSTRING_TO_LANG_LITERAL, true)
+                  .set(BasicWriterSettings.XSD_STRING_TO_PLAIN_LITERAL, true)
+                  .set(BasicWriterSettings.INLINE_BLANK_NODES, true);
+            try {
+                writer.startRDF();
+                writer.handleNamespace("hmas", "https://purl.org/hmas/core/");
+                writer.handleNamespace("td", "https://www.w3.org/2019/wot/td#");
+                writer.handleNamespace("htv", "http://www.w3.org/2011/http#");
+                writer.handleNamespace("hctl", "https://www.w3.org/2019/wot/hypermedia#");
+                writer.handleNamespace("wotsec", "https://www.w3.org/2019/wot/security#");
+                writer.handleNamespace("dct", "http://purl.org/dc/terms/");
+                writer.handleNamespace("js", "https://www.w3.org/2019/wot/json-schema#");
+                writer.handleNamespace("saref", "https://w3id.org/saref#");
+                graph.forEach(writer::handleStatement);
+                writer.endRDF();
+            } catch (final RDFHandlerException e) {
+                this.failed(e.getMessage());
+                throw new RuntimeException(e);
+            }
+            return out.toString(StandardCharsets.UTF_8);
+        } catch (final IOException e) {
+            this.failed(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @INTERNAL_OPERATION
+    private void signalResponseReceived() {
+        this.requestComplete = true;
+    }
+
+    @GUARD
+    private boolean isRequestComplete() {
+        return this.requestComplete;
+    }
+}
